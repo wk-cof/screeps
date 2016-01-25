df72138e453dde8528c6ba8eacbd7be43e75dfee
Mikhail Gorbachev <mgorbachev@factset.com>
2016-01-25 14:58:26 -0500

    build all the things

--- src/bulider.ts
+++ src/bulider.ts
@@ -1,13 +1,25 @@
 /// <reference path="../typings/tsd.d.ts" />
 
 export interface IBuilder{
-    buildRoad: () => void;
+    buildRoad:(creep:Creep, spawn:Spawn) => void;
     upgradeController:(creep:Creep, spawn:Spawn) => void;
 }
 
 var builder = {
-    buildRoad: () => {
-
+    buildRoad: (creep:Creep, spawn:Spawn) => {
+        if (creep.carry.energy == 0){// creep.carryCapacity) {
+            if (spawn.transferEnergy(creep) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(spawn);
+            }
+        }
+        else {
+             var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+            if (targets.length) {
+                if (creep.upgradeController(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0]);
+                }
+            }
+        }
     },
     upgradeController : (creep:Creep, spawn:Spawn) => {
         if (creep.carry.energy == 0){// creep.carryCapacity) {
--- src/main.ts
+++ src/main.ts
@@ -5,29 +5,53 @@ var harvester = require('harvester');
 var builder:IBuilder = require('builder');
 
 
-function buildCreep(bodyParts, spawnName, name, memory) {
-    return Game.spawns[spawnName].createCreep(bodyParts, name, memory);
+module Build{
+     function buildCreep(bodyParts, spawnName, name, memory) {
+        return Game.spawns[spawnName].createCreep(bodyParts, name, memory);
 
-}
-
-function buildBuilder(spawnName, name) {
-    return buildCreep([WORK, WORK, CARRY, MOVE], spawnName, name, {role: 'builder'});
-}
-
-function isLegalCreepName(name){
-    return _.isUndefined(Memory.creeps[name]);
-}
-
-function buildWorker(spawnName) {
-    let templateName = 'Worker';
-    let index = 1;
-    while(!isLegalCreepName(templateName+index)){
-        index++;
     }
-    let newName = templateName+index;
 
-    if( _.isObject(buildCreep([WORK, CARRY, MOVE], spawnName, newName, {role: 'worker'})) ){
-        console.log('Successfully Created a new worker: ' + newName);
+    function isLegalCreepName(name){
+        return _.isUndefined(Memory.creeps[name]);
+    }
+
+    export function buildCreepType(type, spawnName){
+        let templateName = type;
+        let index = 1;
+        while(!isLegalCreepName(templateName+index)){
+            index++;
+        }
+        let newName = templateName+index;
+
+        if( _.isObject(buildCreep([WORK, CARRY, MOVE], spawnName, newName, {role: type})) ){
+            console.log('Successfully Created a new builder: ' + newName);
+        }
+    }
+
+    export function buildBuilder(spawnName) {
+        let templateName = 'Builder';
+        let index = 1;
+        while(!isLegalCreepName(templateName+index)){
+            index++;
+        }
+        let newName = templateName+index;
+
+        if( _.isObject(buildCreep([WORK, CARRY, MOVE], spawnName, newName, {role: 'builder'})) ){
+            console.log('Successfully Created a new builder: ' + newName);
+        }
+    }
+
+    export function buildWorker(spawnName) {
+        let templateName = 'Worker';
+        let index = 1;
+        while(!isLegalCreepName(templateName+index)){
+            index++;
+        }
+        let newName = templateName+index;
+
+        if( _.isObject(buildCreep([WORK, CARRY, MOVE], spawnName, newName, {role: 'worker'})) ){
+            console.log('Successfully Created a new worker: ' + newName);
+        }
     }
 }
 
@@ -46,20 +70,29 @@ module.exports.loop = function () {
 
     let creepNames:string[] = _.keys(Memory.creeps);
     let workers:string[] = _.filter(creepNames, (creepName) => creepName.match(/worker/i));
+    let builders:string[] = _.filter(creepNames, (creepName) => creepName.match(/builder/i));
+    let upgraders:string[] = _.filter(creepNames, (creepName) => creepName.match(/upgrader/i));
+
     if (workers.length < 3) {
-        buildWorker(spawn1);
+        Build.buildCreepType('worker', spawn1);
+    } else if (builders.length < 1) {
+        Build.buildCreepType('builder', spawn1);
+    } else if (upgraders.length < 2) {
+        Build.buildCreepType('upgrader', spawn1);
     }
 
 
     // let creeps : MyCreeps = <MyCreeps> Game.creeps;
     for (let name in Game.creeps) {
         var creep = Game.creeps[name];
-        //if (creep.memory['role'] === 'builder') {
-        //    builder.upgradeController(creep, spawn1);
-        //}
-        //else
-        if (creep.memory['role'] == 'worker') {
+        if (creep.memory['role'] === 'upgrader') {
+            builder.upgradeController(creep, Game.spawns[spawn1]);
+        }
+        else if (creep.memory['role'] == 'worker') {
             harvester(creep, spawn1);
+        }
+        else if (creep.memory['role'] == 'builder') {
+            builder.buildRoad(creep, Game.spawns[spawn1]);
         }
     }
     return null;
