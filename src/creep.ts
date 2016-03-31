53b58f2148b60dc63c8ca2f033ad1c97d3683588
Mikhail Gorbachev <mgorbachev@factset.com>
2016-03-31 19:58:15 -0400

    added doOrMoveTo2 that should fulfill the requred functionality

--- src/creep.ts
+++ src/creep.ts
@@ -114,6 +114,27 @@ export class MyCreep implements IMyCreep {
         }
     }
 
+    /**
+     * Updated version of doOrMoveTo that allows to move and do an action at the same time.
+     * @param action Action to perform
+     * @param target Target on which to perform action
+     * @param distance Optional parameter to specify the distance at which to start performing action. Default is 1
+     */
+    protected doOrMoveTo2(action:Function, target:Structure|Creep|ConstructionSite|Source, distance?:number):number {
+        if (!distance) {
+            distance = 1;
+        }
+        let rangeToTarget = this.creep.pos.getRangeTo(target);
+        if (rangeToTarget <= distance) {
+            return action.call(this.creep, target);
+        }
+        if (rangeToTarget <= (distance+1)) {
+            this.creep.moveTo(target);
+            return action.call(this.creep, target);
+        }
+        return this.creep.moveTo(target);
+    }
+
     protected findClosestByRange<T>(findConstant:number, filterFunction?:Function):T {
         if (filterFunction) {
             return this.creep.pos.findClosestByRange(findConstant, {
